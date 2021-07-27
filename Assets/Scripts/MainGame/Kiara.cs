d341ec658bc2148fdaddb7c3b6adf76239522943
LAPTOP-I5T6N59F\user <cobra20621@gmail.com>
2021-07-27 17:25:51 +0800

    初步Kiara模型

---
+++ Assets/Scripts/MainGame/Kiara.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Lean.Localization;
+
+public class Kiara : MonoBehaviour
+{
+    public Animator animator;
+
+    public GameObject kiaraBubble;
+    public Text lab_speech;
+    
+    // 0:idle 1: 
+    public void SetKiaraAnime(string trigger){
+        animator.SetTrigger(trigger);
+    }
+
+    public void SetKiaraText(string text){
+        SetBubbleActive(true);
+        lab_speech.text = LeanLocalization.GetTranslationText(text);
+    }
+
+    public void SetBubbleActive(bool bo){
+        kiaraBubble.SetActive(true);
+    }
+}
+
---
+++ Assets/Scripts/MainGame/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Kiara kiara;
+    public string info = "Kiara/Intro1";
+    public string anime = "exciting";
+
+    [ContextMenu("Say")]
+    public void Say(){
+        kiara.SetKiaraText(info);
+    }
+
+    [ContextMenu("SetAnime")]
+    public void SetAnime(){
+        kiara.SetKiaraAnime(anime);
+    }
+
+}
