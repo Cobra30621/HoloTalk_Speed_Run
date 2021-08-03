30cf9620677ba245e92fbaf0c308781ac96d704e
John1276 <sihua.john@gmail.com>
2021-08-03 23:18:52 +0800

    修好了Start的Bug,增加StopScene與切換場景功能

---
+++ Assets/Scripts/StopSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StopSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //按下空白鍵返回Game
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //直接複製並修改即可按下空白鍵即暫停
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
