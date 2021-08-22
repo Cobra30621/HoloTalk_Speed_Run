bc08686e07d9c9c29e6190a249e9dccb14a29d4a
John1276 <sihua.john@gmail.com>
2021-08-22 14:51:55 +0800

    新增了OptionUI檔案,將部分Option的程式碼移入

---
+++ Assets/Scripts/UI/OptionUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OptionUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+    //控制全螢幕
+    public void OnClick(bool IsOn)
+    {
+        if(IsOn)
+        {
+            print("On");
+            Screen.fullScreen=true;
+        }
+        else
+        {
+            print("Off");
+            Screen.fullScreen=false;
+        }
+    }
+}
