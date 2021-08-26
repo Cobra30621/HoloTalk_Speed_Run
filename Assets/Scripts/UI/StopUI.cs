656bd989e4baf5fe93acc87a11bcdc823fbedd64
LAPTOP-I5T6N59F\user <cobra20621@gmail.com>
2021-08-26 12:06:41 +0800

    製作暫停介面，更改字體

---
+++ Assets/Scripts/UI/StopUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StopUI : MonoBehaviour
+{
+    [SerializeField] private GameObject optionPanel;
+    [SerializeField] public BoardPanel boardPanel;
+
+    public void ShowStopPanel(){
+        boardPanel.ShowPanel();
+    }
+
+    public void CloseStopPanel(){
+        boardPanel.ClosePanel();
+    }
+
+    public void ShowOptionPanel(){
+        optionPanel.SetActive(true);
+    }
+
+    public void CloseOptionPanel(){
+        optionPanel.SetActive(false);
+    }
+
+    public void Again(){
+        SceneManager.LoadScene("Game");
+    }
+
+    public void BackToTitle(){
+        SceneManager.LoadScene("StartScene");
+    }
+}
