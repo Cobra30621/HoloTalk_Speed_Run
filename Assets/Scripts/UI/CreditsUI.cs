7921733e62903a38a1f18bff0e4045b9cb35b0e9
John1276 <sihua.john@gmail.com>
2021-08-28 00:43:29 +0800

    搞定CreditsScene跟Option的字型,弄了個醜醜的按鈕進CreditScene

---
+++ Assets/Scripts/UI/CreditsUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Lean.Localization;
+public class CreditsUI : MonoBehaviour
+{
+    public BoardPanel boardPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowCreditsUI(){
+        boardPanel.ShowPanel();
+    }
+
+    public void CloseOptionUI(){
+        boardPanel.ClosePanel();
+        // SaveSettings();
+    }
+}
