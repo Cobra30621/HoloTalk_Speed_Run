f1f93383720ac11f4104f8d763f7d2012e216da7
LAPTOP-I5T6N59F\user <cobra20621@gmail.com>
2021-08-31 19:24:14 +0800

    會動的Kiara

---
+++ Assets/Scripts/MainGame/KiaraToucher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KiaraToucher : MonoBehaviour
+{
+    
+    public Kiara kiara;
+    public SFXManager sFX;
+    public KiaraAction[] kiaraActions;
+
+    public void OnClick(){
+        int f = Random.Range(0, kiaraActions.Length);
+        sFX.PlaySFX((int) kiaraActions[f].kiaraSFX);
+        kiara.SetKiaraAnime(kiaraActions[f].kiaraState);
+        StartCoroutine(BackToNormal());
+    }
+
+    IEnumerator BackToNormal(){
+        yield return new WaitForSeconds(1f);
+        kiara.SetKiaraAnime(KiaraState.Idle);
+    }
+
+}
+
+[System.Serializable]
+public struct KiaraAction{
+    public KiaraState kiaraState;
+    public KiaraSFX kiaraSFX;
+}
